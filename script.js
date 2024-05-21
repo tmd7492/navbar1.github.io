a86310a1340f3e58e12314b9811797aeca6cb5ad
tmd7492 <168692060+tmd7492@users.noreply.github.com>
2024-05-21 15:07:58 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('#mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
